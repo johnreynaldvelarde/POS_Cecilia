3a7b2331b293a97de492e2bc7fa9b2a19619c301
Admin <Admin@DESKTOP-STAD1GF>
2023-06-10 11:06:59 +0800

    new update login form

--- POS_SYSTEM_Cecilia_Ukay_Ukay/Login_Form.cs
+++ POS_SYSTEM_Cecilia_Ukay_Ukay/Login_Form.cs
@@ -25,7 +25,7 @@ namespace POS_SYSTEM_Cecilia_Ukay_Ukay
 
         private string name = "Admin";
         private string pass = "12345";
-        private string role = "Manager";
+        private string role = "Default";
 
         public void Clear()
         {
@@ -37,22 +37,30 @@ namespace POS_SYSTEM_Cecilia_Ukay_Ukay
         // login button to main form
         private void btnLogin_Click(object sender, EventArgs e)
         {
-           // Main_Form frm = new Main_Form();
-           // frm.Show();
-            
+            string inputtedUsername = txtUsername.Text;
+            string inputtedPassword = txtPassword.Text;
+
             if (String.IsNullOrEmpty(txtUsername.Text) || String.IsNullOrEmpty(txtPassword.Text))
             {
                 MessageBox.Show("Please enter both Staff Name and Password.", "Input Required", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+            else if (inputtedUsername == name && inputtedPassword == pass)
+            {
+                Account_Class account = new Account_Class();
+                account.Staff_Name = name;
+                account.Staff_Role = role;
+
+                Main_Form mainForm = new Main_Form();
+                mainForm.btn_Sell.Enabled = false;
+                mainForm.Show();
+            }
             else 
             {
-
                 using (SqlConnection connect = new SqlConnection(database.MyConnection()))
                 {
                     connect.Open();
 
-
-                    string sql = "SELECT Staff_ID, Staff_Image, Staff_Name, Role, Password FROM Staff_Account WHERE Staff_Name = @username";
+                    string sql = "SELECT Staff_ID, Staff_Image, Staff_Name, Role, Password FROM Staff_Account WHERE Staff_Name = @username and Archive = 0";
 
                     using (SqlCommand command = new SqlCommand(sql, connect))
                     {
@@ -88,6 +96,11 @@ namespace POS_SYSTEM_Cecilia_Ukay_Ukay
                                     else if (account.Staff_Role == "Cashier")
                                     {
                                         Main_Form cashier = new Main_Form();
+                                        cashier.btn_Inventory.Enabled = false;
+                                        cashier.btn_Archive.Enabled = false;
+                                        cashier.btn_History.Enabled = false;
+                                        cashier.btn_Settings.Enabled = false;
+                                        cashier.btn_User.Enabled = false;
                                         // Rest of the code for the cashier role
                                     }
                                     else
@@ -106,105 +119,9 @@ namespace POS_SYSTEM_Cecilia_Ukay_Ukay
                             }
                         }
                     }
-                    // string sql = "SELECT Staff_ID, Staff_Image, Staff_Name, Role FROM Staff_Account WHERE Staff_Name = @username AND Password = @password ";
-                    // string sql = "SELECT Staff_ID, Staff_Image, Staff_Name, Role FROM Staff_Account WHERE LOWER(Staff_Name) = LOWER(@username) AND Password = @password";
-                    /*
-                     string sql = "SELECT Staff_ID, Staff_Image, Staff_Name, Role FROM Staff_Account WHERE Staff_Name = @username COLLATE Latin1_General_CS_AS AND Password = @password COLLATE Latin1_General_CS_AS";
-
-                     using (SqlCommand command = new SqlCommand(sql, connect))
-                     {
-                         command.Parameters.AddWithValue("@username", txtUsername.Text.ToString());
-                         command.Parameters.AddWithValue("@password", txtPassword.Text.ToString()); 
-
-                         using (SqlDataReader reader = command.ExecuteReader())
-                         {
-                             if (reader.Read())
-                             {
-
-                                 Account_Class account = new Account_Class();
-
-                                 account.Staff_ID = reader.GetInt32("Staff_ID");
-
-                                 byte[] imageData = (byte[])reader["Staff_Image"];
-                                 using (MemoryStream ms = new MemoryStream(imageData))
-                                 {
-                                     account.Staff_Image = Image.FromStream(ms);
-                                 }
-                                 account.Staff_Name = reader.GetString(2);
-                                 account.Staff_Role = reader.GetString(3);
-
-                                 if (account.Staff_Role == "Manager")
-                                 {
-
-                                     Main_Form manager = new Main_Form();
-                                     manager.user_profile.Image = account.Staff_Image;
-                                     manager.Show();
-
-                                 }
-                                 else if (account.Staff_Role == "Cashier")
-                                 {
-                                     Main_Form cashier = new Main_Form();
-
-                                 }
-                                 else
-                                 {
-                                     // Handle other roles or show an error message for unrecognized roles
-                                 }
-                             }
-                             else
-                             {
-                                 MessageBox.Show("Invalid Staff Name and Password");
-                             }
-                         }
-                     }
-                     */
-
+                   
                 }
             }
-
-             /*
-            Main_Form frm = new Main_Form();
-            frm.Show();
-            this.Hide(); //this hides the login Form after before the the main form opens
-
-            // Sa ulit muna ilagay ang code para mabilis buksan
-            //  "Data Source=COLA\\SQLEXPRESS;Initial Catalog=cecila;Integrated Security=True"  --dapiton string connections
-         
-            SqlConnection con = new SqlConnection("Data Source=COLA\\SQLEXPRESS;Initial Catalog=cecila;Integrated Security=True");
-            SqlCommand cmd = new SqlCommand();
-            con.Open();
-            cmd.Connection = con;
-            cmd.CommandText = "select * from tbl_admin";
-            SqlDataReader dr = cmd.ExecuteReader();
-
-            if (dr.Read())
-            {
-                 if (userBox.Text.Equals(dr["userName"].ToString()) && passBox.Text.Equals(dr["password01"].ToString()))
-                {
-
-                    MessageBox.Show("Login Success");
-
-
-
-
-
-
-                }
-                else { 
-                
-                MessageBox.Show("Login Failed");
-                }
-
-                
-
-            }
-            else {
-
-
-                MessageBox.Show("No data found, create account pls");
-            
-            }
-            */
         }
 
         private void Login_Form_Load(object sender, EventArgs e)
