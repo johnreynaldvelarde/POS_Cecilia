6e7a3541328a093d54f11bb23ed493fd7ed22a17
Guze <Guze@LENOVO-PC>
2023-06-10 20:21:20 +0800

    new update

--- POS_SYSTEM_Cecilia_Ukay_Ukay/DB_Connection.cs
+++ POS_SYSTEM_Cecilia_Ukay_Ukay/DB_Connection.cs
@@ -11,7 +11,7 @@ namespace POS_SYSTEM_Cecilia_Ukay_Ukay
     {
         public string MyConnection()
         {
-            string dapitonCn = @"Data Source=COLA\SQLEXPRESS;Initial Catalog=DB_Cecilia;Integrated Security=True";
+           // string con = @"Data Source=COLA\SQLEXPRESS;Initial Catalog=DB_Cecilia;Integrated Security=True";
             string con = @"Data Source=LENOVO-PC\SQLEXPRESS;Initial Catalog=DB_Cecilia;Integrated Security=True";// database in velarde
             //string con = @"Data Source=DESKTOP-STAD1GF\SQLEXPRESS;Initial Catalog=DB_Cecilia;Integrated Security=True"; // database in velarde in another device
             return con;
