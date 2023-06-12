c29fbd0f9275a0b50a641aca2b862a209c702f3a
caesar0016 <94184309+caesar0016@users.noreply.github.com>
2023-06-12 17:53:16 +0800

    Add files via upload

---
+++ POS_SYSTEM_Cecilia_Ukay_Ukay/loadingBar.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace POS_SYSTEM_Cecilia_Ukay_Ukay
+{
+    partial class loadingBar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressPanel = new System.Windows.Forms.Panel();
+            this.time01 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(110)))), ((int)(((byte)(140)))));
+            this.panel1.Controls.Add(this.progressPanel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 185);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(622, 28);
+            this.panel1.TabIndex = 0;
+            // 
+            // progressPanel
+            // 
+            this.progressPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(69)))), ((int)(((byte)(140)))));
+            this.progressPanel.Location = new System.Drawing.Point(0, 0);
+            this.progressPanel.Name = "progressPanel";
+            this.progressPanel.Size = new System.Drawing.Size(50, 28);
+            this.progressPanel.TabIndex = 0;
+            // 
+            // time01
+            // 
+            this.time01.Interval = 15;
+            this.time01.Tick += new System.EventHandler(this.time01_Tick);
+            // 
+            // loadingBar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(622, 213);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "loadingBar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "loadingBar";
+            this.Load += new System.EventHandler(this.loadingBar_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel progressPanel;
+        private System.Windows.Forms.Timer time01;
+    }
+}
---
+++ POS_SYSTEM_Cecilia_Ukay_Ukay/loadingBar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_SYSTEM_Cecilia_Ukay_Ukay
+{
+    public partial class loadingBar : Form
+    {
+        public loadingBar()
+        {
+            InitializeComponent();
+        }
+
+        private void time01_Tick(object sender, EventArgs e)
+        {
+            progressPanel.Width += 3;
+
+            if (progressPanel.Width >= 700) {
+
+                time01.Stop();
+                Login_Form logs = new Login_Form();
+                logs.Show();
+                this.Hide();
+            
+            
+            }
+        
+        
+        }
+
+        private void loadingBar_Load(object sender, EventArgs e)
+        {
+            time01.Start();
+        }
+    }
+}
